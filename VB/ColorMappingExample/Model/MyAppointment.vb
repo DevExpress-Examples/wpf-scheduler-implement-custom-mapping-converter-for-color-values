fa56eb05ce2b4e8393bf06ceb5849d89271237a3
DevExpressExampleBot <fakeEmail>
2022-12-17 05:06:24 +0400

    Source auto update [skip ci]

--- VB/ColorMappingExample/Model/MyAppointment.vb
+++ VB/ColorMappingExample/Model/MyAppointment.vb
@@ -1,17 +1,15 @@
-﻿Imports DevExpress.Mvvm.POCO
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
+Imports DevExpress.Mvvm.POCO
 
 Namespace ColorMappingExample.Model
+
     Public Class MyAppointment
+
         Public Shared Function Create() As MyAppointment
             Return ViewModelSource.Create(Function() New MyAppointment())
         End Function
+
         Friend Shared Function Create(ByVal startTime As Date, ByVal endTime As Date, ByVal subject As String, ByVal categoryId As Integer) As MyAppointment
-            Dim apt As MyAppointment = MyAppointment.Create()
+            Dim apt As MyAppointment = Create()
             apt.StartTime = startTime
             apt.EndTime = endTime
             apt.Subject = subject
@@ -21,16 +19,27 @@ Namespace ColorMappingExample.Model
 
         Protected Sub New()
         End Sub
-        Public Overridable Property Id() As Integer
-        Public Overridable Property AllDay() As Boolean
-        Public Overridable Property StartTime() As Date
-        Public Overridable Property EndTime() As Date
-        Public Overridable Property Subject() As String
-        Public Overridable Property StatusId() As Integer
-        Public Overridable Property CategoryId() As Integer
-        Public Overridable Property Type() As Integer
-        Public Overridable Property Location() As String
-        Public Overridable Property RecurrenceInfo() As String
-        Public Overridable Property ReminderInfo() As String
+
+        Public Overridable Property Id As Integer
+
+        Public Overridable Property AllDay As Boolean
+
+        Public Overridable Property StartTime As Date
+
+        Public Overridable Property EndTime As Date
+
+        Public Overridable Property Subject As String
+
+        Public Overridable Property StatusId As Integer
+
+        Public Overridable Property CategoryId As Integer
+
+        Public Overridable Property Type As Integer
+
+        Public Overridable Property Location As String
+
+        Public Overridable Property RecurrenceInfo As String
+
+        Public Overridable Property ReminderInfo As String
     End Class
 End Namespace
